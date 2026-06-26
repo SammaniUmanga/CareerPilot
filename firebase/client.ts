b78908359b8db12cfd0402933a14995c59ef0fc7
SammaniUmanga <umangasam16@gmail.com>
2026-06-26 16:22:48 +0530

    remove apikey

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,13 +3,13 @@ import {getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCpimqugsNGl7Dbsjel0xUwJzceB6G1ADo",
-    authDomain: "careerpilot-6fce5.firebaseapp.com",
-    projectId: "careerpilot-6fce5",
-    storageBucket: "careerpilot-6fce5.firebasestorage.app",
-    messagingSenderId: "72256288981",
-    appId: "1:72256288981:web:617ecefcf5d8684c640a5c",
-    measurementId: "G-QC76L4TFLK"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 // Initialize Firebase

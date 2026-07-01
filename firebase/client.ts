cecc1fb7b071352e708c3f16ce7766e4d763b220
SammaniUmanga <umangasam16@gmail.com>
2026-07-01 21:29:32 +0530

    Config apikey change

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,7 +3,7 @@ import {getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
+    apiKey: process.env.FIREBASE_API_KEY || "AIzaSyCpimqugsNGl7Dbsjel0xUwJzceB6G1ADo",
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
